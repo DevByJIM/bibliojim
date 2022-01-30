98a77fce3cbeaef33d46a8feb2d5f8691c0a97dc
DevByJIM <correo.jimartinez@gmail.com>
2022-01-30 19:02:16 +0100

    iniciamos el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { ListbookComponent } from './components/listbook/listbook.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path:'', component: AppComponent},
+  {path:'contact', component: ContactComponent},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AuthorsComponent,
+    ListbookComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
