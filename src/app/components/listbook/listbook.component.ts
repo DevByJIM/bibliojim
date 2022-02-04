c70ed57ac0e84d9b7329982884d5fba79b6bd0e0
DevByJIM <correo.jimartinez@gmail.com>
2022-02-05 00:57:56 +0100

    creando el acordeon

--- src/app/components/listbook/listbook.component.ts
+++ src/app/components/listbook/listbook.component.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked  } from '@angular/core';
 import { Libro } from 'src/app/models/libro';
 import { LibroService } from './../../services/libro.service';
 
@@ -8,15 +8,28 @@ import { LibroService } from './../../services/libro.service';
   templateUrl: './listbook.component.html',
   styleUrls: ['./listbook.component.css']
 })
-export class ListbookComponent implements OnInit {
+export class ListbookComponent implements OnInit,AfterViewChecked {
 
   public libros: Libro[] = [];
 
+  
+  
+  
   constructor(private _libroService: LibroService) {
-
+    
+  }
+  ngAfterViewChecked(): void {
+    this.cargarAcordeon();
+  }
+  
+  ngOnInit(): void {
+    
+    this.cargarDatos();
   }
 
-  ngOnInit(): void {
+  
+
+  cargarDatos():void{
     this._libroService.listadoLibros().subscribe(
       {
         next: (v) => {
@@ -27,18 +40,28 @@ export class ListbookComponent implements OnInit {
           console.error(e);
         },
         complete: () => {
-          console.info('Completo');          
+          console.info('Completo');        
         }
       }
     );
-    // .subscribe(
-    //   response =>{
 
-    //   },
-    //   error =>{
 
-    //   }
-    // );
+  }
+
+  cargarAcordeon(): void{
+    const bloque = document.querySelectorAll('.bloque')
+    const bloque_header = document.querySelectorAll('.bloque__header');
+
+
+    bloque_header.forEach( (obj, i) =>{     
+      bloque_header[i].addEventListener('click',()=>{
+          bloque.forEach((content, i)=>{
+            bloque[i].classList.remove('activo');
+          })
+         bloque[i].classList.add('activo');
+       })
+    });
+
   }
 
 }
